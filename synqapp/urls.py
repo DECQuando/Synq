f83adcd3608ad43860eba6f2795c35d7f2092a41
SHIO777 <shiotatsu1015@gmail.com>
2023-01-22 18:46:56 +0900

    [update] welcome/upload/albumページを追加

--- synqapp/urls.py
+++ synqapp/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
 from . import views
 
+app_name = 'synqapp'
+
+
 urlpatterns = [
-    path("synqapp/", views.index),
-    path("upload/", views.upload_page, name="upload_page"),
+    # path("", views.index),
+    path("", views.welcome_page, name="welcome"),
+    path("synqapp/album", views.album_page, name="album"),
+    path("synqapp/upload", views.upload_page, name="upload"),
 ]
--- synqapp/views.py
+++ synqapp/views.py
@@ -6,7 +6,23 @@ from django.http import HttpResponse
 def index(request):
     return HttpResponse("Hello Django!!")
 
-class SampleView(View):
+
+class WelcomeView(View):
+    def get(self, request, *args, **kwargs):
+        return render(request, "synqapp/welcome.html")
+
+
+class AlbumView(View):
+    def get(self, request, *args, **kwargs):
+        return render(request, "synqapp/album.html")
+
+
+class UploadView(View):
     def get(self, request, *args, **kwargs):
         return render(request, "synqapp/upload.html")
-upload_page = SampleView.as_view()
+
+
+welcome_page = WelcomeView.as_view()
+album_page = AlbumView.as_view()
+upload_page = UploadView.as_view()
+
