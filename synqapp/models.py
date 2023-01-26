f7cc70c1e68b8b1723ca7ba6fa1f4b788adadbbd
SHIO777 <shiotatsu1015@gmail.com>
2023-01-26 13:08:49 +0900

    [update] models.pyにコメント追加

--- synqapp/models.py
+++ synqapp/models.py
@@ -10,5 +10,7 @@ class Image(models.Model):
     created_at = models.DateTimeField(default=timezone.now, verbose_name='作成日')
     name = models.CharField(max_length=100)
 
+
     def __str__(self):
+        """管理者画面での表示形式を定義"""
         return "作成日: {} 作成者: {}".format(self.created_at, self.name)
