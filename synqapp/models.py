bdd8aa670a376308dea01fdcc901ac41034f0d1c
SHIO777 <shiotatsu1015@gmail.com>
2023-01-26 13:02:28 +0900

    [update] Imageモデルを作成

--- synqapp/models.py
+++ synqapp/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.utils import timezone
+
 
 # Create your models here.
+class Image(models.Model):
+    """
+    画像データ
+    """
+    created_at = models.DateTimeField(default=timezone.now, verbose_name='作成日')
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return "作成日: {} 作成者: {}".format(self.created_at, self.name)
