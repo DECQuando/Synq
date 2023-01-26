ea3bcf1240c47cbbe4895b7f7243481b7cdd060f
SHIO777 <shiotatsu1015@gmail.com>
2023-01-26 14:57:39 +0900

    [add] 名前を投稿するアプリを作成 [update] クラス汎用ビューを導入

--- synqapp/models.py
+++ synqapp/models.py
@@ -8,7 +8,7 @@ class Image(models.Model):
     画像データ
     """
     created_at = models.DateTimeField(default=timezone.now, verbose_name='作成日')
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, verbose_name="名前")
 
 
     def __str__(self):
