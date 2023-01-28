8f19610132c4627851d080863578fbd35efaaba9
SHIO777 <shiotatsu1015@gmail.com>
2023-01-28 09:54:06 +0900

    [update] 不要なファイル・行を削除

--- synqapp/views.py
+++ synqapp/views.py
@@ -11,31 +11,12 @@ from .forms import ImageForm
 from django.urls import reverse_lazy
 
 
-def index(request):
-    return HttpResponse("Hello Django!!")
-
-
 class WelcomeView(View):
     def get(self, request, *args, **kwargs):
         return render(request, "synqapp/welcome.html")
 
 
-class AlbumView(View):
-    def get(self, request, *args, **kwargs):
-        # 回数分写真を繰り返し表示する
-        pics_num = [i for i in range(10)]
-        context = {'pics_num': pics_num, }
-        return render(request, "synqapp/album.html", context)
-
-
-class UploadView(View):
-    def get(self, request, *args, **kwargs):
-        return render(request, "synqapp/upload.html")
-
-
 welcome_page = WelcomeView.as_view()
-album_page = AlbumView.as_view()
-upload_page = UploadView.as_view()
 
 
 class CSRFExemptMixin(object):
