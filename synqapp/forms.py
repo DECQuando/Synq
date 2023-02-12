5194cbb7d6bf811be1a68e5d32c1cff84517a603
SHIO777 <shiotatsu1015@gmail.com>
2023-02-12 13:06:58 +0900

    [update] グルーピングアルゴリズムを関数化

--- synqapp/forms.py
+++ synqapp/forms.py
@@ -7,6 +7,43 @@ import numpy as np
 from PIL import Image as PILImage
 
 
+def calculate_distance(path1: str, path2: str) -> float:
+    """
+    2つの画像のベクトル間の距離を算出する関数
+    :param path1: Path for the first image
+    :param path2: Path for the second image
+    :return: Vector distance between two images
+    """
+    # pathから画像データを取得
+    img1 = cv2.imread(path1)
+    img2 = cv2.imread(path2)
+    # imgsimライブラリで画像をベクトル化
+    vtr = imgsim.Vectorizer()
+    vec1 = vtr.vectorize(img1)
+    vec2 = vtr.vectorize(img2)
+    # 2つのベクトル間の距離を算出
+    dist = imgsim.distance(vec1, vec2)
+    return dist
+
+
+def return_group(distance: int, previous_image_group: int, max_distance: int) -> int:
+    """
+    グループの番号を返す関数
+    :param distance: Vector distance between two images
+    :param previous_image_group: Group of the previous image
+    :param max_distance: Maximum distance to be considered as the same group
+    :return: Group number of the uploaded image
+    """
+    if distance <= max_distance:
+        print("same: ", previous_image_group)
+        print("dist: ", distance)
+        return previous_image_group
+    else:
+        print("next: ", previous_image_group+1)
+        print("dist: ", distance)
+        return previous_image_group + 1
+
+
 class ImageForm(forms.ModelForm):
     class Meta:
         model = Image
@@ -22,8 +59,6 @@ class ImageForm(forms.ModelForm):
             latest_data = Image.objects.latest('created_at')
             # 一つ前の投稿のグループを取得
             latest_group = latest_data.group
-            # 一つ前の投稿の画像URLを取得し，BASE_DIRと結合
-            img_latest_path = base_dir + latest_data.image.url
             # 初めての画像かを示すflag
             is_first_pic = 0
         else:
@@ -36,42 +71,14 @@ class ImageForm(forms.ModelForm):
         obj.group = latest_group
         obj.save()
 
-        # 1つ前の画像データがあれば
+        # 1つ前の画像データがあれば比較
         if is_first_pic == 0:
             # 一つ前の画像データを取得
-            # 画像データをCV2で数値化
-            img_latest = cv2.imread(img_latest_path)
-            # imgsimライブラリで画像をベクトル化
-            vtr = imgsim.Vectorizer()
-            vec_img_latest = vtr.vectorize(img_latest)
-
+            img_latest_path = base_dir + latest_data.image.url
             # 投稿された画像データを取得
             img_uploaded_path = base_dir + obj.image.url
-            img_uploaded = cv2.imread(img_uploaded_path)
-            vec_img_uploaded = vtr.vectorize(img_uploaded)
-            dist = imgsim.distance(vec_img_latest, vec_img_uploaded)
-            if dist <= 10:
-                group = latest_group
-                print("group_same: ", group)
-            else:
-                group = latest_group + 1
-                print("group_next: ", group)
-            print("dist: ", dist)
+            dist = calculate_distance(path1=img_latest_path, path2=img_uploaded_path)
+            group = return_group(distance=dist, previous_image_group=latest_group, max_distance=10)
             obj.group = group
             obj.save()
         return obj
-
-        # formの初期値を設定
-    # def __init__(self, *args, **kwargs):
-    #     super(ImageForm, self).__init__(*args, **kwargs)
-    #     # TODO: 画像の類似度を評価
-    #     self.instance.group = 13
-
-        # img_pil = PILImage.open(self.instance.image)
-        # img = np.asarray(np.array(img_pil))
-        # print(img)
-
-    # fieldsにあるフィールドは以下で書き換え可能
-    # def clean(self):
-    #     self.cleaned_data["name"] = "Mr. Hello"
-    #     return self.cleaned_data
