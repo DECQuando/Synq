499c6b38d5c6d810e49bef3281036a680a791afa
kneg <k.kaneko@cmt.phys.kyushu-u.ac.jp>
2023-02-15 17:11:21 +0900

    [add] 新規登録後の自動ログイン機能を実装

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,6 @@
 from django.urls import reverse_lazy
 from django.views import generic
-from django.contrib.auth import get_user_model
+from django.contrib.auth import get_user_model, login, authenticate
 
 from .forms import SignUpForm
 
@@ -9,5 +9,13 @@ User = get_user_model()
 
 class SignUpView(generic.CreateView):
     form_class = SignUpForm
-    success_url = reverse_lazy('accounts:login')
+    success_url = reverse_lazy('synqapp:welcome')
     template_name = 'accounts/signup.html'
+
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        email = form.cleaned_data.get('email')
+        password = form.cleaned_data.get('password1')
+        user = authenticate(email=email, password=password)
+        login(self.request, user)
+        return response
