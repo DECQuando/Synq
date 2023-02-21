86fd4accfc280e1c0f89bbc350496cf4d0e71f17
SHIO777 <shiotatsu1015@gmail.com>
2023-02-21 12:45:13 +0900

    [update] toastでプッシュ通知を表示

--- Synq/settings.py
+++ Synq/settings.py
@@ -149,17 +149,17 @@ else:
 # debug toolbar #
 #################
 
-if DEBUG:
-    def show_toolbar(request):
-        return True
-
-
-    INSTALLED_APPS += (
-        'debug_toolbar',
-    )
-    MIDDLEWARE += (
-        'debug_toolbar.middleware.DebugToolbarMiddleware',
-    )
-    DEBUG_TOOLBAR_CONFIG = {
-        'SHOW_TOOLBAR_CALLBACK': show_toolbar,
-    }
+# if DEBUG:
+#     def show_toolbar(request):
+#         return True
+#
+#
+#     INSTALLED_APPS += (
+#         'debug_toolbar',
+#     )
+#     MIDDLEWARE += (
+#         'debug_toolbar.middleware.DebugToolbarMiddleware',
+#     )
+#     DEBUG_TOOLBAR_CONFIG = {
+#         'SHOW_TOOLBAR_CALLBACK': show_toolbar,
+#     }
